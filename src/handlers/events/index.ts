642d18a7737f24b36307c5c69e44e6d986fc371d
Raul Glogovetan <raul@glogovetan.com>
2023-09-27 20:59:44 +0300

    Add: `Event` entity handler

---
+++ src/handlers/events/index.ts
@@ -0,0 +1,44 @@
+import { DataHandlerContext, Log } from '@subsquid/evm-processor'
+import { Store } from '@subsquid/typeorm-store'
+import { Event, Block, Transaction } from '../../model'
+
+const handleEvent = async (
+  context: DataHandlerContext<Store, {}>,
+  log: Log & { transactionHash: string; logIndex: number; },
+  decodedLog: any,
+  eventName: string,
+) => {
+  const event = new Event({ id: `${log.transactionHash}_event_${log.logIndex}`});
+
+  let block = await context.store.get(Block, { where: { id: `block_${log.block.height}` } });
+  if (!block) {
+    block = new Block({ id: `block_${log.block.height}`});
+    block.timestamp = BigInt(log.block.timestamp);
+    await context.store.insert(block);
+  }
+
+  let transaction = await context.store.get(Transaction, { where: { id: log.transactionHash }});
+  if (!transaction) {
+    transaction = new Transaction({ id: log.transactionHash });
+    transaction.block = block;
+    await context.store.insert(transaction);
+  }
+
+  event.transaction = transaction;
+  event.address = log.address;
+  // todo: event.associatedColony
+  event.timestamp = BigInt(log.block.timestamp);
+  event.name = eventName;
+
+  let args = decodedLog.toObject();
+  for (const [key, value] of Object.entries(args)) {
+    if (typeof value === 'bigint') {
+      args[key] = value.toString();
+    }
+  }
+  event.args = JSON.stringify(args);
+
+  await context.store.insert(event);
+};
+
+export default handleEvent;
